5726f04e1f1467d88e07839bc0be794ca7214b3b
telichko1 <46589288+telichko1@users.noreply.github.com>
2025-07-01 13:35:28 +1100

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,22 +1,4 @@
-import time
-import threading
-import requests
 from main import run_bot
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
-    # Запускаем Flask в отдельном потоке
-    flask_thread = threading.Thread(target=run_flask, daemon=True)
-    flask_thread.start()
-    
-    # Запускаем бота в основном потоке
     run_bot()
